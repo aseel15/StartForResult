f09adb7833e786943b39fdf455e0bb8fa6cacd98
Aseel Zbaidah <aseel.zbai@gmail.com>
2021-12-06 03:19:11 +0200

    sec

--- app/src/main/java/edu/cs/startforresult/MainActivity.java
+++ app/src/main/java/edu/cs/startforresult/MainActivity.java
@@ -11,16 +11,21 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
     int req_Code=1;
+    int req_Code2=2;
     TextView txtMsg;
+    TextView txtMsg2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtMsg=findViewById(R.id.txtMsg);
+        txtMsg2=findViewById(R.id.txtMsg2);
     }
     public void btnGoOnClick(View view) {
         startActivityForResult(new Intent(this, MainActivity2.class), req_Code);
     }
+
+
     public void onActivityResult(int reqCode, int resultCode, Intent data){
         if(reqCode==req_Code){
             if(resultCode==RESULT_OK){
@@ -29,5 +34,17 @@ public class MainActivity extends Activity {
                 txtMsg.setText(data.getData().toString());
             }
         }
+        if(reqCode==req_Code2){
+            if(resultCode==RESULT_OK){
+
+                // Toast.makeText(this, data.getData().toString(), Toast.LENGTH_SHORT);
+                txtMsg2.setText(data.getData().toString());
+            }
+        }
+
+    }
+
+    public void btnGoOnClick2(View view) {
+        startActivityForResult(new Intent(this, MainActivity3.class), req_Code2);
     }
 }
